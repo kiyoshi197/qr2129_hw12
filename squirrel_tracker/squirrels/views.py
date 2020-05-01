c1902974121d42fe6e9dc29786e5f375465c587f
Qingqing Ren <qren429@gmail.com>
2020-05-01 21:01:49 +0000

    Add views and urls

--- squirrel_tracker/squirrels/views.py
+++ squirrel_tracker/squirrels/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import Httpresponse
+
+
+def index(request):
+    return HttpResponse("Hi This is a  squirrels tracker app.")
 
 # Create your views here.
