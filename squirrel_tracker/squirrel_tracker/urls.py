2ecbdffce856fdb1a58e76a387be434b618b4904
Qingqing Ren <qren429@gmail.com>
2020-05-02 01:17:52 +0000

    Update urls

--- squirrel_tracker/squirrel_tracker/urls.py
+++ squirrel_tracker/squirrel_tracker/urls.py
@@ -14,10 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('map/', views.map, name='map' ),
+    path('map/', include('squirrels.urls')),
 ]
