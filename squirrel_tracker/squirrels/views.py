c5521a34d4e4ec0f0f54b8b72116ce3f3c15d394
Zt-Wan <zw2617@columbia.edu>
2020-05-05 06:07:37 +0000

    register Sighting

--- squirrel_tracker/squirrels/views.py
+++ squirrel_tracker/squirrels/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from django.utils import timezone
 
 from .models import Sighting 
 
@@ -22,5 +23,15 @@ def list_sightings(request):
 
 def get_sighting(request, squirrel_id):
     squirrel = Sighting.objects.get(unique_squirrel_id = squirrel_id)
-    return render(request, 'squirrels/sighting.html', {'squirrel':squirrel})
- 
+    return render(request, 'squirrels/sightings.html', {'squirrel':squirrel})
+
+def add_sighting(request, longitude, latitude,unique_squirrel_id,  shift, date, age):
+    s = Sighting(latitude = float(latitude),
+            longitude = float(longitude),
+            unique_squirrel_id = float(unique_squirrel_id),
+            shift = shift,
+            date = date,
+            age = age
+            )
+
+    return render(request, 'squirrels/sightings.html', {'squirrel':squirrel} )
