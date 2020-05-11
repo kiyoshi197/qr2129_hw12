6b1e2664d13dc64e94e706bdba9abe5c2c0000b4
Qingqing Ren <qren429@gmail.com>
2020-05-11 04:38:59 +0000

    update sighting detail page

--- squirrel_tracker/squirrels/urls.py
+++ squirrel_tracker/squirrels/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('',views.index,name='index'),
     path('map/',views.map, name = "map"),
     path('sightings/', views.list_sightings, name = 'list_sightings'),
-    path('sighintgs/<slug:squirrel_id>/', views.get_sighting, name = 'get_sighting'),
-    path('sightings/add', views.get_sighting, name = 'add_sighting'),
+    path('<str:squirrel_id>/', views.get_sighting, name = 'get_sighting'),
+    path('sightings/add/', views.get_sighting, name = 'add_sighting'),
+    path('stats/', views.stats, name ='stats'),
 ]
